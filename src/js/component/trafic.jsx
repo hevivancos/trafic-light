059176b056743e99b60813c5da5136461f96e5c6
hevivancos <vivancos.ramon@gmail.com>
2022-02-28 11:58:29 +0000

    acabando

---
+++ src/js/component/trafic.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Trafic = () => {
+	const [color, setColor] = useState(null);
+
+	return (
+		<div className="container">
+			<div
+				className={`red ${color === "red" ? "shadow-circle" : null}`}
+				onClick={() => setColor("red")}></div>
+
+			<div
+				className={`green ${
+					color === "green" ? "shadow-circle" : null
+				}`}
+				onClick={() => setColor("green")}></div>
+
+			<div
+				className={`yellow ${
+					color === "yellow" ? "shadow-circle" : null
+				}`}
+				onClick={() => setColor("yellow")}></div>
+		</div>
+	);
+};
+
+export default Trafic;
